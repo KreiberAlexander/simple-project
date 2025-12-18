7e3cf19cfdd181a4c03fa387fbc731db067c2ad3
Alexander Kreiber <git@github.com:KreiberAlexander/simple-project.git>
2025-12-18 11:03:39 +0100

    add html

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-console.log("Server is starting...");
+console.log("Hello");
 
-console.log("Loading configuration...");
-const config = require('./config');
-console.log("Configuration loaded:", config);
-console.log("Initializing database connection...");
-console.log("Database connected.");
+// git restore <name_file> - сбросит все изменения до последнего добавления
+
+// git stash - спрячет все измения с последнего коммита
+// git stash pop - вернуть измения
+
+// Единоразовая настройка системы, делаем один раз для компьютера
+
+// git config --global user.name "Change Me"
+// git config --global user.email "changeme@gmail.com"
+// git config --global pull.rebase true
+// git config --list - покажет что получилось
+
+// gitignore
+
+// Как работать по ветки
+// git branch <name> - создать ветку из текущей
+
+// создать и переключиться в ветку
+// git checkout -b <name_branch>
+
+// переключиться в ветку main
+// git checkout <name_branch>
+// git checkout main
+
+// создать ветку new-branch из ветки existing-branch
+// git branch <new-branch> <existing-branch>
+
+// например, создать ветку футер из ветки home_page
+// git branch footer home_page
