ae4dd607a366825f8f7476006e1fef9d0e310ed5
Kreiber <ekkel.alexander@gmail.com>
2025-12-18 10:05:46 +0100

    fix

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ console.log("Server is starting...");
 console.log("Loading configuration...");
 const config = require('./config');
 console.log("Configuration loaded:", config);
-git restore
+console.log("Initializing database connection...");
